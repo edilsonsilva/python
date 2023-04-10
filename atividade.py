d064c06b154d4436e60168d0fb8967e4973f7f60
Edilson <edilson@uol.com.br>
2023-04-10 09:02:22 -0300

    Init:Inicialização de códigos em Python

---
+++ atividade.py
@@ -0,0 +1,15 @@
+x = None
+y = None
+p = None
+
+#realizar a entrada de dados com o comando input
+x = input("Digite um valor: ")
+y = input("Digite outro valor: ")
+
+p = int(x) + int(y)
+print(p)
+p = float(x) + float(y)
+print(p)
+#realizar o calculo de potencia exp.2
+print(pow(int(x),2))
+print(pow(int(y),2))
